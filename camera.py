1650285ce9ae2960bf068ed0ab35be84e2e487da
kanak-ag <133563970+kanak-ag@users.noreply.github.com>
2024-01-17 03:33:04 +0530

    Add files via upload

--- camera.py
+++ camera.py
@@ -1,14 +1,29 @@
-from flask import Flask, render_template, Response
-from camera import Video 
-app=Flask(__name__)
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-@app.route('/video')
-def video():
-    return Response(Video)
-
-app.run(debug=True)
+import cv2
 
 
+faceDetect=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+class Video(object):
+    def __init__(self):
+        self.video=cv2.VideoCapture(0)
+    def __del__(self):
+        self.video.release()
+    def get_frame(self):
+        ret,frame=self.video.read()
+        faces=faceDetect.detectMultiScale(frame, 1.3, 5)
+        for x,y,w,h in faces:
+            x1,y1=x+w, y+h
+            cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,255), 1)
+            cv2.line(frame, (x,y), (x+30, y),(255,0,255), 6) #Top Left
+            cv2.line(frame, (x,y), (x, y+30),(255,0,255), 6)
+
+            cv2.line(frame, (x1,y), (x1-30, y),(255,0,255), 6) #Top Right
+            cv2.line(frame, (x1,y), (x1, y+30),(255,0,255), 6)
+
+            cv2.line(frame, (x,y1), (x+30, y1),(255,0,255), 6) #Bottom Left
+            cv2.line(frame, (x,y1), (x, y1-30),(255,0,255), 6)
+
+            cv2.line(frame, (x1,y1), (x1-30, y1),(255,0,255), 6) #Bottom right
+            cv2.line(frame, (x1,y1), (x1, y1-30),(255,0,255), 6)
+        ret,jpg=cv2.imencode('.jpg',frame)
+        return jpg.tobytes()
